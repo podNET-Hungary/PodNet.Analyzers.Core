a5c50a11940b0e8aaaec9d188d453be04c4c56d4
Gábor Szabó <szabo.gabor@podnet.hu>
2024-02-08 15:28:50 +0100

    Update PathProcessingTests to only compare to BLC implementation (xplat)

--- tests/Analyzers.Tests/PathProcessingTests.cs
+++ tests/Analyzers.Tests/PathProcessingTests.cs
@@ -6,23 +6,21 @@ namespace PodNet.Analyzers.Tests;
 public class PathProcessingTests
 {
     [DataTestMethod]
-    [DataRow(".", @"C:\Users\source\Project1", @"C:\Users\source\Project1")]
-    [DataRow(".", @"C:\Users\source\Project1", @"C:\Users\source\Project1\")]
-    [DataRow(".", @"C:\Users\source\Project1\", @"C:\Users\source\Project1\")]
-    [DataRow(@"C:\Users\source\Project1\", @"D:\Users\source\Project1", @"C:\Users\source\Project1\")]
-    [DataRow("Items", @"C:\Users\source\Project1", @"C:\Users\source\Project1\Items")]
-    [DataRow(@"Items\", @"C:\Users\source\Project1", @"C:\Users\source\Project1\Items\")]
-    [DataRow(@"Item.cs", @"C:\Users\source\Project1", @"C:\Users\source\Project1\Item.cs")]
-    [DataRow(@"Items\Item", @"C:\Users\source\Project1", @"C:\Users\source\Project1\Items\Item")]
-    [DataRow(@"Items\Item.cs", @"C:\Users\source\Project1", @"C:\Users\source\Project1\Items\Item.cs")]
-    [DataRow(@"..\..", @"C:\Users\source\Project1", @"C:\Users\")]
-    [DataRow(@"..\Users", @"C:\User", @"C:\Users")]
-    [DataRow(@"..\..\Users\source\", @"C:\User\source", @"C:\Users\source\")]
-    public void GetRelativePath_WorksAsExpected(string expected, string relativeTo, string path)
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\source\Project1")]
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\source\Project1\")]
+    [DataRow(@"C:\Users\source\Project1\", @"C:\Users\source\Project1\")]
+    [DataRow(@"D:\Users\source\Project1", @"C:\Users\source\Project1\")]
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\source\Project1\Items")]
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\source\Project1\Items\")]
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\source\Project1\Item.cs")]
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\source\Project1\Items\Item")]
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\source\Project1\Items\Item.cs")]
+    [DataRow(@"C:\Users\source\Project1", @"C:\Users\")]
+    [DataRow(@"C:\User", @"C:\Users")]
+    [DataRow(@"C:\User\source", @"C:\Users\source\")]
+    public void GetRelativePath_WorksAsExpected(string relativeTo, string path)
     {
-        var bclActual = Path.GetRelativePath(relativeTo, path);
-        Assert.AreEqual(expected, bclActual);
-
+        var expected = Path.GetRelativePath(relativeTo, path);
         var actual = PathProcessing.GetRelativePath(relativeTo, path);
         Assert.AreEqual(expected, actual);
     }
